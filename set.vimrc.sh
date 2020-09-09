1435a846c132688a5c5a57925381c547efdda160
zundamoti01 <zundamoti01@gmail.com>
2020-09-09 15:19:14 +0900

    Add:vimrc設置用スクリプトの追加

--- set.vimrc.sh
+++ set.vimrc.sh
@@ -1,17 +1,34 @@
 #!/bin/sh
+
 target=$HOME/
 vimrc=.vimrc
-echo "${target}に名前 ${vimrc} でシンボリックリンクを作成します。"
-if [ -e $target$vimrc ]; then
-    echo ".vimrcは存在しています。削除して置き換えますか？(Y/N)"
-    read resp
-    if [ $resp = "Y" ]; then
-        echo "Yが入力されました。"
-    elif [ $resp = "N" ]; then
-        echo "Nが入力されました。"
-    else
-        echo "Y/Nを入力してください"
-    fi
-fi
 
+create_link() {
+    current_dir=$(cd $(dirname $0); pwd)
+    echo $current_dir
+    ln -s $current_dir/.vimrc ${target}${vimrc}
+    echo "${target}${vimrc}にシンボリックリンクを作成しました！"
+}
+
+echo "${target}に名前 ${vimrc} でシンボリックリンクを作成します。"
+while true
+do
+    if [ -e $target$vimrc ]; then
+        echo ".vimrcは存在しています。削除して置き換えますか？(Y/N)"
+        read resp
+        if [ $resp = "Y" ]; then
+            echo "Yが入力されました。"
+            rm $target$vimrc
+            echo "${target}${vimrc}が削除されました。"
+	    create_link && break
+        elif [ $resp = "N" ]; then
+            echo "Nが入力されました。" && break
+        else
+            echo "Y/Nを入力してください"
+        fi
+    else
+        create_link
+        break
+    fi
+done
 
