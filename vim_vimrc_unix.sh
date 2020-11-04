43e3c0aed056108498704c937f2c52731fc769be
BuildTools <unconfigured@null.spigotmc.org>
2020-11-04 23:47:19 +0900

    Update:vim-lspのPluginInstallが実行されるように変更

--- vim_vimrc_unix.sh
+++ vim_vimrc_unix.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-target=$HOME/
+TARGET_DIR=$HOME/
 vimrc=.vimrc
 
 create_link() {
     current_dir=$(cd $(dirname $0); pwd)
     echo $current_dir
-    ln -s $current_dir/.vimrc ${target}${vimrc}
-    echo "${target}${vimrc}にシンボリックリンクを作成しました！"
+    ln -s $current_dir/.vimrc ${TARGET_DIR}${vimrc}
+    echo "${TARGET_DIR}${vimrc}にシンボリックリンクを作成しました！"
 }
 
-echo "${target}に名前 ${vimrc} でシンボリックリンクを作成します。"
+echo "${TARGET_DIR}に名前 ${vimrc} でシンボリックリンクを作成します。"
 while true
 do
-    if [ -e $target$vimrc ]; then
+    if [ -e $TARGET_DIR$vimrc ]; then
         echo ".vimrcは存在しています。削除して置き換えますか？(Y/N)"
         read resp
         if [ $resp = "Y" ]; then
             echo "Yが入力されました。"
-            rm $target$vimrc
-            echo "${target}${vimrc}が削除されました。"
+            rm $TARGET_DIR$vimrc
+            echo "${TARGET_DIR}${vimrc}が削除されました。"
 	    create_link && break
         elif [ $resp = "N" ]; then
             echo "Nが入力されました。" && break
@@ -35,6 +35,13 @@ done
 # vim-plugのインストール
 VIMPLUG=~/.vim/autoload/plug.vim
 if [ ! -e $VIMPLUG ]; then
-curl -fLo $VIMPLUG --create-dirs \
+    curl -fLo $VIMPLUG --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    # vim-plug PlugInstallの実行
+    if vim -c :PlugInstall -c :q! -c :q!; then
+        echo "vim-plugのプラグインのインストールが完了しました。"
+    else
+        echo "vim-plugのプラグインインストールに失敗しました。"
+    fi
 fi
+
