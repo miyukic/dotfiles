4c02e3e31b7e95bf2c8a97a0b72fdecbc0b8e531
miyukic <m.yukic10@gmail.com>
2021-12-11 06:22:31 +0900

    profile_placement.batの修正

--- PowerShell/profile_placement.bat
+++ PowerShell/profile_placement.bat
@@ -1,10 +1,18 @@
 @echo off
-rem PowerSHellのMicrosoft.PowerShell_profile.ps1を配置します
+chcp 65001
+rem "PowerSHellのMicrosoft.PowerShell_profile.ps1を配置します"
 set PROFILE_NAME=Microsoft.PowerShell_profile.ps1
 
-echo 現在のディレクトリパス:
+echo "現在のディレクトリパス:"
 echo %~dp0
-echo 参照先のプロファイルのファイル名:%PROFILE_NAME%
+echo "参照先のプロファイルのファイル名:%PROFILE_NAME%"
 
-mklink /D %USERPROFILE%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 %~dp0\%PROFILE_NAME%
+set LINK_FILE_PATH=%USERPROFILE%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+
+IF not exist %LINK_FILE_PATH% (
+    mklink %LINK_FILE_PATH% %~dp0\%PROFILE_NAME%
+) else (
+    echo "すでにファイルが存在しています"
+)
+
 @pause
