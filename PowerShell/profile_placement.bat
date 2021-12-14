155f757b959b75e7c33017a75cdc32f851d30c52
miyukic <m.yukic10@gmail.com>
2021-12-14 14:09:18 +0900

    update

--- PowerShell/profile_placement.bat
+++ PowerShell/profile_placement.bat
@@ -5,10 +5,11 @@ set PROFILE_NAME=Microsoft.PowerShell_profile.ps1
 
 echo "現在のディレクトリパス:"
 echo %~dp0
-echo "参照先のプロファイルのファイル名:%PROFILE_NAME%"
+echo "参照先のプロファイルのファイル名:"%PROFILE_NAME%
 
 set LINK_FILE_PATH=%USERPROFILE%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 
+rem リンクファイルが存在するかどうか
 IF not exist %LINK_FILE_PATH% (
     mklink %LINK_FILE_PATH% %~dp0\%PROFILE_NAME%
 ) else (
