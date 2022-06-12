23f5787b0b53b5e7a50a453f9e3bb874152088ab
miyukic <m.yukic10@gmail.com>
2022-06-12 23:59:38 +0900

    update

--- PowerShell/starship_install.bat
+++ PowerShell/starship_install.bat
@@ -1,23 +1,35 @@
 rem @echo off
 chcp 65001
-rem scoopコマンド存在確認
+@rem scoopコマンド存在確認
 
 WHERE /Q scoop
-IF %ERRORELEVEL% == 0 (
-    scoop install starship
-) ELSE (
+echo %ERRORELEVEL%
+IF "%ERRORELEVEL%" == "1" (
     echo "scoopコマンドがありません"
-    exit
+    echo "starshipをインストールするにはscoopが必要です"
+    rem scoopのインストール
+    echo ==================================================
+    echo scoopのインストールを開始します....
+    powershell -c Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+    rem scoopのインストール
+    powershell -c iwr -useb get.scoop.sh | iex && echo scoopのインストールが完了しました。
+
 )
+echo "starshipをインストールします"
+scoop install starship
 
-IF NOT EXIST %USERPROFILE%\.config (
-    mkdir %USERPROFILE%\.config
 
-    setlocal
-    for /f "usebackq delims=" %%A in (`ver`) do set PATH=%%A
-    echo %PATH%
 
-    rem mklink %USERPROFILE%\.config\starship.toml %~dp0
-) ELSE (
-    echo ".configフォルダが既に存在しているため作成できませんでした"
-)
+@rem IF NOT EXIST %USERPROFILE%\.config (
+@rem     mkdir %USERPROFILE%\.config
+@rem 
+@rem     setlocal
+@rem     for /f "usebackq delims=" %%A in (`ver`) do set PATH=%%A
+@rem     echo %PATH%
+@rem 
+@rem     rem mklink %USERPROFILE%\.config\starship.toml %~dp0
+@rem ) ELSE (
+@rem     echo ".configフォルダが既に存在しているため作成できませんでした"
+@rem )
+
+pwsh ./starship_install.ps1
---
+++ PowerShell/test/hoge.bat
@@ -0,0 +1,2 @@
+chcp 65001
+pwsh hoge.ps1
