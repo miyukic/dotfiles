9650040a8c5b089dca41e5b0fa8566c3a849db01
miyukic <m.yukic10@gmail.com>
2022-06-22 19:26:39 +0900

    setup.ps1 scoopで複数のコマンドを自動インストール

--- PowerShell/setup.ps1
+++ PowerShell/setup.ps1
@@ -8,39 +8,62 @@ function IsExistCommand([string] $command) {
     return $?
 }
 
+
 [Boolean] $result = IsExistCommand("pwsh")
 if ($result) { #存在する場合
-    echo "PowerShell(pwsh)はインストール済みです"
+    Write-Output "PowerShell(pwsh)はインストール済みです"
 } else { #存在しない場合
     winget install PowerShell.Windows
     $result = IsExistCommand("pwsh")
     if ($result) { 
-        echo "powershell(pwsh)のインストールに失敗しました"
+        Write-Output "powershell(pwsh)のインストールに失敗しました"
         exit
     }
 }
 
 $result = IsExistCommand("scoop")
 if ($result) { 
-    echo "scoopはインストール済みです"
+    Write-Output "scoopはインストール済みです"
 } else { 
     Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force iwr -useb get.scoop.sh | iex
     $result = IsExistCommand("scoop")
     if ($result) {
-        echo "scoopのインストールに失敗しました"
+        Write-Output "scoopのインストールに失敗しました"
         exit
     }
 }
 
-$installExtraBuckets=@("extras","versions","github-gh")
-$notexists = $installExtraBuckets | 
-Where-Object {(scoop bucket list | select Name | ForEach-Object Name) -notcontains $_ }
-$notexists | % {echo "scoopに $_ bucket を追加します"; scoop bucket add $_ }
 
-$installCommandList=@("7zip","git","curl","fzf","git","gsudo","lsd","neovim","vim-nightly","starship","winfetch")
-#micro, ripgrep
+echo "scoopのバケットを追加処理します"
+[string[]]$installExtraBuckets = @("extras","versions","github-gh")
+[string[]]$notexists = @()
+$installExtraBuckets | Where-Object {
+    if ((scoop bucket list | Select-Object Name | ForEach-Object Name) -notcontains $_) {
+        $notexists += $_
+    }
+}
+$notexists | ForEach-Object {Write-Output "scoopに $_ bucket を追加します"; scoop bucket add $_ }
 
-$installCommandList | ForEach-Object { 
-    $result=IsExistCommand($_); if ($result -eq $false) {scoop install $_}
+$installCommandList=@(
+    "7zip","git","curl","fzf","git", "gsudo","lsd","neovim","vim-nightly","starship","winfetch", "less"
+)
+
+#micro, ripgrep, lessmsi, gow
+
+#$installCommandList | ForEach-Object { 
+#    $result=IsExistCommand($_); if ($result -eq $false) {scoop install $_}
+#}
+
+[string[]]$notexists = @()
+$scoopList = $(scoop list)
+$installCommandList | ForEach-Object {
+    if (($scoopList | Select-Object Name | ForEach-Object Name) -notcontains $_) {
+        $notexists += $_
+    }
+}
+
+$notexists | ForEach-Object {
+    Write-Output "$_をインストールします"
+    scoop install $_
 }
 
