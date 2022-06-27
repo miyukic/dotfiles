cdb7ffd27083b2348ecf3d508a16416269480295
miyukic <m.yukic10@gmail.com>
2022-06-27 19:25:54 +0900

    bootstrap.shの追加

--- bash/set-bashrc.sh
+++ bash/set-bashrc.sh
@@ -6,25 +6,30 @@ REAL_ALIASES_FILE="bash_aliases"
 REAL_MYBASHRC_FILE="mybashrc"
 
 printLine() {
+    echo -ne "\e[1;33m"
     lines=$(tput cols)
     for ((i=0; i<$lines*1; i++)); do
         echo -n ─ 
     done
     echo
+    echo -ne "\e[1;0m"
 }
 
 printLine
 echo " "
 
+# .bash_aliasesの配置
 if [ -e $HOME/$ALIASES_FILE ]; then
     echo ${ALIASES_FILE}"はすでに存在しています"
-    echo "${ALIASES_FILE}は存在しています。削除して置き換えますか？(Y/N)"
+    echo 
+    echo "削除して置き換えますか？(Y/N)"
     read resp
     if [ $resp = "Y" ]; then
         echo "Yが入力されました。"
-        rm "~/"${ALIASES_FILE}
+        rm $HOME/$ALIASES_FILE
         #echo "~/"${ALIASES_FILE}"が削除されました。"
-        sudo ln -s `pwd`"/"${REAL_ALIASES_FILE} $HOME"/.bash_aliases"
+        sudo ln -s `pwd`"/"${REAL_ALIASES_FILE} $HOME"/.bash_aliases" && echo "`pwd`/${REAL_ALIASES_FILE} ==> ~/$ALIASES_FILE シンボリックリンクをを作成しました"
+
     elif [ $resp = "N" ]; then
         echo "Nが入力されました。";
         echo "${ALIASES_FILE}は置き換えません" #&& break
@@ -40,15 +45,18 @@ echo " "
 printLine
 echo " "
 
+# .mybashrcの配置
 if [ -e $HOME/$MYBASHRC_FILE ]; then
     echo ${MYBASHRC_FILE}"はすでに存在しています"
-    echo "${MYBASHRC_FILE}は存在しています。削除して置き換えますか？(Y/N)"
-    read resp
-    if [ $resp = "Y" ]; then
+    echo 
+    echo "削除して置き換えますか？(Y/N)"
+    read resp2
+    if [ $resp2 = "Y" ]; then
         echo "Yが入力されました。"
-        rm ~/$MYBASHRC_FILE
+        rm $HOME/$MYBASHRC_FILE
         #echo "~/ ${MYBASHRC_FILE} が削除されました。"
-        sudo ln -s "`pwd`/${REAL_MYBASHRC_FILE}}" ~/.bash_aliases
+        sudo ln -s "`pwd`/${REAL_MYBASHRC_FILE}}" ~/${MYBASHRC_FILE} && echo "`pwd`/${REAL_MYBASHRC_FILE} ==> ~/${MYBASHRC_FILE} シンボリックリンクを作成しました"
+
     elif [ $resp = "N" ]; then
         echo "Nが入力されました。";
         echo "${MYBASHRC_FILE}は置き換えません" #&& break
@@ -64,6 +72,7 @@ echo " "
 printLine
 echo " "
 
+# .bashrc内に.mybashrcの読み込みスクリプトの追記
 cat ~/.bashrc | grep -E "^source ~/${MYBASHRC_FILE}$"
 if [ $? = 1 ]; then
     echo "source ~/${MYBASHRC_FILE}" >> ~/.bashrc
---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo -e "\n" | /bin/bash ./vim_vimrc_unix.sh && \
+/bin/bash ./bash/set-bashrc.sh && \
+/bin/bash ./Git/gitinit.sh
