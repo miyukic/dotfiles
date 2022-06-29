2a1db740654c0f8f221380c4c8f99f077bcb5d69
miyukic <m.yukic10@gmail.com>
2022-06-29 13:07:28 +0900

    git pullを以前の動作にする設定を追加

--- Git/gitinit.sh
+++ Git/gitinit.sh
@@ -7,6 +7,10 @@ if type git > /dev/null 2>&1; then
     git config --global user.email m.yukic10@gmail.com
     git config --global user.name "miyukic"
 
+    # git pull の動作を以前のデフォルトに
+    git config --global pull.rebase false
+    # 自動で改行コードを変更しない
+    git config --global core.autoCRLF false
 
     if type vim > /dev/null 2>&1; then
         git config --global core.editor 'vim -c "set fenc=utf-8"'
