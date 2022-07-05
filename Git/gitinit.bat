7a8a9c90ec1d89b7354bff692324819f8f9c140d
miyukic <m.yukic10@gmail.com>
2022-07-06 02:57:13 +0900

    update gitの日本語ファイル文字化け防止設定

--- Git/gitinit.bat
+++ Git/gitinit.bat
@@ -19,15 +19,19 @@ IF NOT "%ERRORELEVEL%" == "1" (
 git config --global user.email m.yukic10@gmail.com
 echo ☑ user.email m.yukic10@gmail.com
 git config --global user.name "miyukic"
-echo ☑ --global user.name "miyukic"
+echo ☑ user.name "miyukic"
 
 rem git pull の動作を以前のデフォルトに
 git config --global pull.rebase false
 echo ☑ git pull の動作を以前のデフォルトに
+
 rem 自動で改行コードを変更しない
 git config --global core.autoCRLF false
 echo ☑ 自動で改行コードを変更しない
 
+rem "日本語文字化けを回避設定"
+git config --global core.quotepath false
+echo ☑ 日本語文字化けを回避設定
 :VIM_START
 WHERE /Q vim.exe
 IF NOT "%ERRORELEVEL%" == "1" (
