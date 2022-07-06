96860608fe65769b5e9ec48df0bd28ff61070808
zundamoti01 <zundamoti01@gmail.com>
2022-07-06 15:10:28 +0900

    fix bootstrap.sh: fix bootstrap.sh build dotfiles settings script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,6 @@ echo "debug => $1"
 
 echo $(pwd )
 
-echo -e "Y\n" | bash ./vim_vimrc_unix.sh && \
-bash ./bash/set-bashrc.sh && \
-bash ./Git/gitinit.sh
+echo -e "Y\n" | bash ./bootstrap.sh && \
+# bash ./bash/set-bashrc.sh && \
+# bash ./Git/gitinit.sh
