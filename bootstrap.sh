0039c0b75983370ce0a578550df8e0e0d061b9a8
miyuki <33715209+miyukic@users.noreply.github.com>
2022-07-06 15:22:45 +0900

    Fix bootstrap.sh fix setup-vimrx script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,6 @@ echo "debug => $1"
 
 echo $(pwd )
 
-echo -e "Y\n" | bash ./bootstrap.sh
-# bash ./bash/set-bashrc.sh && \
-# bash ./Git/gitinit.sh
+echo -e "Y\n" | bash ./Vim/vim/vimrc-setup.sh && \
+bash ./bash/set-bashrc.sh && \
+bash ./Git/gitinit.sh
