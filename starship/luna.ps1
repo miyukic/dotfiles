e9b182a7c65b6878baec75d0c0d79b56b4437a5e
miyukic <m.yukic10@gmail.com>
2026-03-18 18:00:10 +0900

    🌀 fix: luna.ps1 日本語出力をコメントアウトした理由を追記

--- starship/luna.ps1
+++ starship/luna.ps1
@@ -5,6 +5,7 @@ Param([string]$flag = "true")
 $CPATH=pwd
 
 function create() {
+# Write-Outputの日本語コメントはpowershell 5.1でCP932誤認識されるため削除
 #    Write-Output ".\starship.toml -> ~\.config\starship.tomlにシンボリックリンクを作成します"
     New-Item -Path ~\.config\ -Name starship.toml -Value $CPATH\starship.toml -ItemType SymbolicLink
 #sudo cmd -c mklink.exe ~\.config\starship.toml .\starship.toml ; exit
