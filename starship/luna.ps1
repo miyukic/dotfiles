1846780072024943e808dd7598f8668ae8a0234b
miyukic <m.yukic10@gmail.com>
2026-03-18 17:54:36 +0900

    🌀 fix: luna.ps1 日本語コメント削除

--- starship/luna.ps1
+++ starship/luna.ps1
@@ -5,21 +5,21 @@ Param([string]$flag = "true")
 $CPATH=pwd
 
 function create() {
-        Write-Output ".\starship.toml -> ~\.config\starship.tomlにシンボリックリンクを作成します"
-            New-Item -Path ~\.config\ -Name starship.toml -Value $CPATH\starship.toml -ItemType SymbolicLink
-                #sudo cmd -c mklink.exe ~\.config\starship.toml .\starship.toml ; exit
+#    Write-Output ".\starship.toml -> ~\.config\starship.tomlにシンボリックリンクを作成します"
+    New-Item -Path ~\.config\ -Name starship.toml -Value $CPATH\starship.toml -ItemType SymbolicLink
+#sudo cmd -c mklink.exe ~\.config\starship.toml .\starship.toml ; exit
 }
 
 if ($flag -eq "true") {
-        if (Test-Path "$env:USERPROFILE\.config\starship.toml") {
-                    Remove-Item "$env:USERPROFILE\.config\starship.toml"
-                            create
-                                }
+    if (Test-Path "$env:USERPROFILE\.config\starship.toml") {
+        Remove-Item "$env:USERPROFILE\.config\starship.toml"
+            create
+    }
 } else {
-        if (Test-Path "$env:USERPROFILE\.config\starship.toml") {
-                } else {
-                            create
-                                }
+    if (Test-Path "$env:USERPROFILE\.config\starship.toml") {
+    } else {
+        create
+    }
 }
 
 #Set-StrictMode -Off
