7703e307437f8313a76e8527232f52ea44b3826c
miyukic <m.yukic10@gmail.com>
2026-03-19 19:17:05 +0900

    🌀 fix: vimrc-setup.batのパス取得を%~dp0..に修正

--- Vim/vim/vimrc-setup.bat
+++ Vim/vim/vimrc-setup.bat
@@ -3,9 +3,8 @@ chcp 65001
 
 rem echo %~dp0
 
-SET CD=%~dp0
-rem 実行ディレクトリの末尾から "\vim\" (5文字) を削って、1つ上の階層を取得
-SET VIM=%CD:~0,-5%
+rem スクリプト自身のディレクトリからVimルートを取得
+SET VIM=%~dp0..
 
 if exist %USERPROFILE%\.gvimrc del %USERPROFILE%\.gvimrc
 mklink %USERPROFILE%\.gvimrc %VIM%\gvimrc
